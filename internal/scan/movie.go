1bb0d7fda5fd23449226f53feeae1b26c45f0d92
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2025-07-10 21:33:38 +0700

    chore(scan): remove outdated comments and unused legacy code

--- internal/scan/movie.go
+++ internal/scan/movie.go
@@ -12,30 +12,6 @@ import (
 type MovieStrategy struct{}
 
 func (MovieStrategy) Scan(roots []string) model.MovieOutput {
-	// result := model.MovieOutput{
-	// 	Type:        "raw",
-	// 	GeneratedAt: time.Now(),
-	// }
-
-	// cache := &dirCache{}
-	// groupMap := make(map[string]model.MovieGroup)
-	// for _, root := range roots {
-	// 	scanMovieRoot(root, groupMap, cache)
-	// }
-	// // scanMovieRoot(root, groupMap, cache)
-
-	// var groupNames []string
-	// for name := range groupMap {
-	// 	groupNames = append(groupNames, name)
-	// }
-	// sort.Strings(groupNames)
-
-	// for _, name := range groupNames {
-	// 	result.Data = append(result.Data, groupMap[name])
-	// }
-
-	// fmt.Println("Movies scanned.")
-	// return result
 	cache := &dirCache{}
 	groups := scanGenericGroup[model.MovieEntry, model.MovieGroup](
 		roots,
@@ -74,100 +50,6 @@ func (MovieStrategy) Scan(roots []string) model.MovieOutput {
 		Data:        groups,
 	}
 }
-
-// func scanMovieRoot(root string, groupMap map[string]model.MovieGroup, cache *dirCache) {
-// 	entries := cache.Read(root)
-// 	if entries == nil {
-// 		return
-// 	}
-
-// 	for _, group := range entries {
-// 		if !group.IsDir() {
-// 			continue
-// 		}
-
-// 		groupName := group.Name()
-// 		groupPath := filepath.Join(root, groupName)
-// 		groupItems := scanMovieGroup(groupPath, cache)
-
-// 		if len(groupItems) == 0 {
-// 			continue
-// 		}
-
-// 		groupMap[groupName] = model.MovieGroup{
-// 			Name:  groupName,
-// 			Items: groupItems,
-// 		}
-// 	}
-// }
-
-// func scanMovieGroup(groupPath string, cache *dirCache) []model.MovieEntry {
-// 	entries := cache.Read(groupPath)
-// 	if entries == nil {
-// 		return nil
-// 	}
-
-// 	sort.Slice(entries, func(i, j int) bool {
-// 		return entries[i].Name() < entries[j].Name()
-// 	})
-
-// 	var items []model.MovieEntry
-// 	for _, entry := range entries {
-// 		if !entry.IsDir() {
-// 			continue
-// 		}
-
-// 		titlePath := filepath.Join(groupPath, entry.Name())
-// 		subEntries := cache.Read(titlePath)
-// 		if subEntries == nil {
-// 			continue
-// 		}
-
-// 		children := extractChildren(titlePath, subEntries, cache, "movies")
-// 		itemType := "single"
-// 		if len(children) > 0 {
-// 			itemType = "collection"
-// 		}
-
-// 		item := model.MovieEntry{
-// 			BaseEntry: model.BaseEntry{
-// 				Type:   itemType,
-// 				Name:   entry.Name(),
-// 				Path:   titlePath,
-// 				Status: resolveStatus(subEntries),
-// 				Icon:   findIcon(titlePath, subEntries),
-// 			},
-// 		}
-
-// 		if len(children) > 0 {
-// 			item.Type = "collection"
-// 			item.Items = children
-// 		}
-
-// 		items = append(items, item)
-// 	}
-
-// 	return items
-// }
-
-// func scanMovieTitle(titlePath string, subEntries []os.DirEntry) (model.MovieEntry, bool) {
-// 	children := extractChildren(titlePath, subEntries, cache)
-// 	entryType := "single"
-// 	if len(children) > 0 {
-// 		entryType = "collection"
-// 	}
-
-// 	return model.MovieEntry{
-// 		BaseEntry: model.BaseEntry{
-// 			Type:   entryType,
-// 			Name:   filepath.Base(titlePath),
-// 			Path:   titlePath,
-// 			Status: resolveStatus(subEntries),
-// 			Icon:   findIcon(titlePath, subEntries),
-// 		},
-// 		Items: children,
-// 	}, true
-// }
 
 func extractChildren(parent string, entries []os.DirEntry, cache *dirCache, contentType string) []model.MovieEntry {
 	// Sort child directories
--- internal/scan/tvshow.go
+++ internal/scan/tvshow.go
@@ -12,29 +12,6 @@ import (
 type TVShowStrategy struct{}
 
 func (TVShowStrategy) Scan(roots []string) model.TVShowOutput {
-	// result := model.TVShowOutput{
-	// 	Type:        "raw",
-	// 	GeneratedAt: time.Now(),
-	// }
-
-	// cache := &dirCache{}
-	// groupMap := make(map[string]model.TVShowGroup)
-	// for _, root := range roots {
-	// 	scanTVRoot(root, groupMap, cache)
-	// }
-
-	// var groupNames []string
-	// for name := range groupMap {
-	// 	groupNames = append(groupNames, name)
-	// }
-	// sort.Strings(groupNames)
-
-	// for _, name := range groupNames {
-	// 	result.Data = append(result.Data, groupMap[name])
-	// }
-
-	// fmt.Println("TV Shows scanned.")
-	// return result
 	cache := &dirCache{}
 	groups := scanGenericGroup[model.TVShowEntry, model.TVShowGroup](
 		roots,
@@ -64,75 +41,6 @@ func (TVShowStrategy) Scan(roots []string) model.TVShowOutput {
 		Data:        groups,
 	}
 }
-
-// func scanTVRoot(root string, groupMap map[string]model.TVShowGroup, cache *dirCache) {
-// 	entries := cache.Read(root)
-// 	if entries == nil {
-// 		return
-// 	}
-
-// 	for _, group := range entries {
-// 		if !group.IsDir() {
-// 			continue
-// 		}
-
-// 		groupName := group.Name()
-// 		groupPath := filepath.Join(root, groupName)
-// 		groupItems := scanTVGroup(groupPath, cache)
-
-// 		if len(groupItems) == 0 {
-// 			continue
-// 		}
-
-// 		groupMap[groupName] = model.TVShowGroup{
-// 			Name:  groupName,
-// 			Items: groupItems,
-// 		}
-// 	}
-// }
-
-// func scanTVGroup(groupPath string, cache *dirCache) []model.TVShowEntry {
-// 	entries := cache.Read(groupPath)
-// 	if entries == nil {
-// 		return nil
-// 	}
-
-// 	sort.Slice(entries, func(i, j int) bool {
-// 		return entries[i].Name() < entries[j].Name()
-// 	})
-
-// 	var items []model.TVShowEntry
-// 	for _, entry := range entries {
-// 		if !entry.IsDir() {
-// 			continue
-// 		}
-
-// 		titlePath := filepath.Join(groupPath, entry.Name())
-// 		subEntries := cache.Read(titlePath)
-// 		if subEntries == nil {
-// 			continue
-// 		}
-
-// 		item := model.TVShowEntry{
-// 			BaseEntry: model.BaseEntry{
-// 				Type:   "single",
-// 				Name:   entry.Name(),
-// 				Path:   titlePath,
-// 				Status: resolveStatus(subEntries),
-// 				Icon:   findIcon(titlePath, subEntries),
-// 			},
-// 		}
-
-// 		seasons := extractSeasonNames(subEntries)
-// 		if len(seasons) > 0 {
-// 			item.Seasons = seasons
-// 		}
-
-// 		items = append(items, item)
-// 	}
-
-// 	return items
-// }
 
 func extractSeasonNames(entries []os.DirEntry) []string {
 	var names []string
