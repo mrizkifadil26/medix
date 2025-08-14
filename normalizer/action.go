ead4b992e61057abdcab886bc465fcf6744312dd
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2025-08-14 10:41:03 +0700

    chore(normalizer): remove unused comments

--- normalizer/action.go
+++ normalizer/action.go
@@ -7,27 +7,6 @@ import (
 	"github.com/mrizkifadil26/medix/normalizer/transformer"
 )
 
-//		"unicodeFix":          normalizer.NormalizeUnicode,
-//		"stripExtension":      normalizer.StripExtension,
-//		"stripBrackets":       normalizer.StripBrackets,
-//		"replaceSpecialChars": normalizer.ReplaceSpecialChars,
-//		"collapseDashes":      normalizer.CollapseDashes,
-//		"toLower":             normalizer.ToLower,
-//		"spaceToDash":         normalizer.SpaceToDash,
-//		"removeKnownPrefixes": normalizer.RemoveKnownPrefixes,
-//		"dotToSpace":          normalizer.DotToSpace,
-//		"slugify":             normalizer.Slugify,
-//		"normalizeDashes":     normalizer.NormalizeDashes,
-//	}
-// var DefaultTransformers = transformers.NewTransformerRegistry(
-// 	transformers.UnicodeNormalizer(),
-// 	transformers.SanitizeSymbols(),
-// 	transformers.NormalizeSeparators(),
-// 	transformers.Trim(),
-// 	transformers.Slugify(),
-// 	transformers.RemoveBrackets(),
-// )
-
 type ActionRegistry struct {
 	Transformers *transformer.Registry
 	Extractors   *extractor.Registry
