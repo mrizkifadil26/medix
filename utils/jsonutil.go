135d01a46b9a93b642fdd8cca05154d8d64439d4
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2025-08-17 14:34:04 +0700

    feat(utils): add JSON loader with orderedmap support
    - Introduce LoadJSONOrdered for preserving key order
    - Add recursive conversion helpers (convertIntoOrderedMap, convertValue)
    - Enable ordered traversal and manipulation of JSON data

--- utils/jsonutil.go
+++ utils/jsonutil.go
@@ -10,6 +10,8 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+
+	"github.com/iancoleman/orderedmap"
 )
 
 // LoadJSON reads a JSON file from the given path and decodes it into the provided value.
@@ -27,6 +29,22 @@ func LoadJSON(path string, v any) error {
 	defer f.Close()
 
 	return json.NewDecoder(f).Decode(v)
+}
+
+func LoadJSONOrdered(path string, v *orderedmap.OrderedMap) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	// var tmp map[string]any
+	// if err := json.Unmarshal(data, &tmp); err != nil {
+	// 	return err
+	// }
+
+	// convertIntoOrderedMap(tmp, v)
+	// return nil
+	return v.UnmarshalJSON(data)
 }
 
 // WriteJSON writes the given data as pretty-formatted JSON to the specified file path.
@@ -49,4 +67,28 @@ func WriteJSON(path string, data any) error {
 	enc := json.NewEncoder(file)
 	enc.SetIndent("", "  ")
 	return enc.Encode(data)
+}
+
+// convertIntoOrderedMap fills dst with the contents of src, recursively converting maps
+func convertIntoOrderedMap(src map[string]any, dst *orderedmap.OrderedMap) {
+	for k, val := range src {
+		dst.Set(k, convertValue(val))
+	}
+}
+
+// convertValue converts maps to OrderedMap recursively, arrays to []any containing OrderedMaps if needed
+func convertValue(val any) any {
+	switch v := val.(type) {
+	case map[string]any:
+		omap := orderedmap.New()
+		convertIntoOrderedMap(v, omap)
+		return omap
+	case []any:
+		for i := range v {
+			v[i] = convertValue(v[i])
+		}
+		return v
+	default:
+		return v
+	}
 }
