4e54dbfd4ff21b0ba97afdc9645fc26e2d44ca48
Muhammad Rizki Fadillah <mrizkifadil26@gmail.com>
2025-08-30 15:28:11 +0700

    feat(enricher): add configurable local enricher with filter support
    - Add Config struct to define filters (config.go)
    - Refactor LocalEnricher to accept Config and store filters
    - Replace hardcoded filter list with dynamic allFilters map
    - Apply only allowed filters based on configuration
    - Clean up unused options argument and redundant comments

---
+++ enricher/local/config.go
@@ -0,0 +1,5 @@
+package local
+
+type Config struct {
+	Filters []string `json:"filters,omitempty"`
+}
--- enricher/local/enricher.go
+++ enricher/local/enricher.go
@@ -8,17 +8,23 @@ import (
 	"github.com/mrizkifadil26/medix/utils/jsonpath"
 )
 
-type LocalEnricher struct{}
-
-func (s *LocalEnricher) Name() string {
-	return "local"
+type LocalEnricher struct {
+	config *Config
 }
 
-var filters = []Filter{
+var allFilters = []Filter{
 	MediaFilter{},
 	SubtitlesFilter{},
 	IconFilter{},
 	CollectionFilter{},
+}
+
+func NewLocalEnricher(cfg *Config) *LocalEnricher {
+	return &LocalEnricher{config: cfg}
+}
+
+func (s *LocalEnricher) Name() string {
+	return "local"
 }
 
 func (e *LocalEnricher) Enrich(
@@ -27,23 +33,17 @@ func (e *LocalEnricher) Enrich(
 ) (any, error) {
 	var errs []error
 
-	// Get optional filter argument from options
-	filterArg := ""
-	if options != nil {
-		filterArg = options["filters"]
-	}
-
 	// Build allowed map
 	allowed := map[string]bool{}
-	if filterArg == "" {
-		for _, f := range filters {
+	if len(e.config.Filters) == 0 {
+		for _, f := range allFilters {
 			allowed[f.Name()] = true
 		}
 	} else {
-		for _, f := range filters {
+		for _, f := range allFilters {
 			allowed[f.Name()] = false
 		}
-		for _, name := range strings.Split(filterArg, ",") {
+		for _, name := range e.config.Filters {
 			allowed[strings.TrimSpace(name)] = true
 		}
 	}
@@ -51,7 +51,6 @@ func (e *LocalEnricher) Enrich(
 	// Get items using your Get helper
 	nodes, err := jsonpath.Get(data, "items.#")
 	if err != nil {
-		// items key not found, nothing to enrich
 		return data, nil
 	}
 
@@ -61,7 +60,7 @@ func (e *LocalEnricher) Enrich(
 	}
 
 	for _, item := range items {
-		for _, f := range filters {
+		for _, f := range allFilters {
 			if allowed[f.Name()] {
 				f.Apply(item, &errs)
 			}
